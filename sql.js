6c820014c39f51fc9c3b998b12fbae0126f45962
vinayakolhapure <vinayakolhapure@aol.com>
2016-10-30 04:44:52 -0400

    remote database settings

--- sql.js
+++ sql.js
@@ -3,13 +3,23 @@ var mysql = require('mysql');
 function Connection() {
     this.pool = null;
 
-    this.init = function() {
+    /*this.init = function() {
         this.pool = mysql.createPool({
             connectionLimit: 10,
             host: 'localhost',
 			port:3310,
             user: 'root',
             password: 'root',
+            database: 'hacknc'
+        });
+    };*/
+	this.init = function() {
+        this.pool = mysql.createPool({
+            connectionLimit: 10,
+            host: '127.3.14.2',
+			port:3306,
+            user: 'adminuHuyB91',
+            password: 'u-NgqK8MjdM-',
             database: 'hacknc'
         });
     };
